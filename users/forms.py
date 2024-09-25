6a735558a82c617e603068d583dd6e526fdac0cb
Daniel Choe <dchoe4377@gmail.com>
2024-09-25 15:48:45 -0400

    Fixed errors.

--- users/forms.py
+++ users/forms.py
@@ -1,1 +1,8 @@
- 
+# users/forms.py
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email')
--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('profile/', views.profile, name='profile'),
+    # path('search/', views.search, name="search"),
 ]
