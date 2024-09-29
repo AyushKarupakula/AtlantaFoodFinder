00ab54b78ebe4e46a4266897e06fff385743c236
AyushKarupakula <akarupakula3@gatech.edu>
2024-09-29 00:11:00 -0400

    Improved button and dropdown UI

--- reactfoodfinder/src/components/SearchSection.tsx
+++ reactfoodfinder/src/components/SearchSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './SearchSection.css'; // We'll create this file for custom styles
 
 interface SearchSectionProps {
   onFindFood: () => void;
@@ -7,12 +8,18 @@ interface SearchSectionProps {
 function SearchSection({ onFindFood }: SearchSectionProps) {
   return (
     <div className="search-section">
-      <input type="text" placeholder="Search for restaurants or cuisines..." />
-      <select>
+      <input
+        type="text"
+        placeholder="Search for restaurants or cuisines..."
+        className="search-input"
+      />
+      <select className="cuisine-select">
         <option value="">All Cuisines</option>
         {/* Add more cuisine options here */}
       </select>
-      <button onClick={onFindFood}>Find Food!</button>
+      <button onClick={onFindFood} className="find-food-button">
+        Find Food!
+      </button>
     </div>
   );
 }
