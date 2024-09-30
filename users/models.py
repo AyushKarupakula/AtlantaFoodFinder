e27eadccee672e685dd738f965d74c8bf9d77f5a
Daniel Choe <dchoe4377@gmail.com>
2024-09-30 19:17:32 -0400

    list of favs shows on dashboard.

--- users/models.py
+++ users/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from restaurants.models import Restaurant
+from django.conf import settings
 
 class CustomUser(AbstractUser):
     # Add any additional fields here
     pass
+
+class UserFavorite(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
