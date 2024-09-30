f048afa244cd410238f08358c9fb29f438cc094c
Daniel Choe <dchoe4377@gmail.com>
2024-09-30 15:53:35 -0400

    Added dashboard page

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('login/', views.LoginUserView.as_view(), name='login'),
     path('profile/', views.profile, name='profile'),
+    path('dashboard/', views.dashboard, name='dashboard')
     # path('search/', views.search, name="search"),
 ]
